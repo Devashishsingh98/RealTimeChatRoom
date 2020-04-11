7e819fc8fb2facfc0e8b13ab424518300d6669b4
ag_pranjal <pranjal.ag.1999@gmail.com>
2020-04-11 22:27:43 +0530

    All rooms delete after server close

--- mysite/routing.py
+++ mysite/routing.py
@@ -1,6 +1,9 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_url_patterns
 from django.urls import path
+from chat.models import GroupMessage
+
+list(map(GroupMessage.delete, GroupMessage.objects.all()))
 
 application = ProtocolTypeRouter({
     "websocket":URLRouter(websocket_url_patterns)
