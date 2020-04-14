05bbc1dfec1d8529abf41778c9f9c466fcd29f64
ag_pranjal <pranjal.ag.1999@gmail.com>
2020-04-14 21:03:21 +0530

    Added username display while chatting

--- chat/views.py
+++ chat/views.py
@@ -25,4 +25,7 @@ def index(request):
 
 @login_required
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name":room_name})
+    return render(request, "chat/room.html", {
+        "room_name":room_name,
+        "username":request.user.username
+        })
